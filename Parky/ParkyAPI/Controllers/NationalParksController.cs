a9d374c2f04b21a23e3b8d525813bee80f411b7a
Pankaj Rayal <https://github.dxc.com/>
2021-02-28 11:39:37 +0530

    Implementing CRUD Operation

---
+++ Parky/ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,119 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")] //api/NationalParks
+    [ApiController]
+    public class NationalParksController : ControllerBase
+    {
+        private INationalParkRepository _npRepo;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var parkList = _npRepo.GetNationalParks();
+            var parkDto = new List<NationalParkDto>();
+
+            foreach (var obj in parkList)
+            {
+                parkDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+
+            return Ok(parkDto);
+        }
+
+        [HttpGet("{nationalParkId:int}", Name = "GetNationalPark")]
+        public IActionResult GetNationalPark(int nationalParkId)
+        {
+            var park = _npRepo.GetNationalPark(nationalParkId);
+            if (park == null)
+            {
+                return NotFound();
+            }
+
+            var parkDto = _mapper.Map<NationalParkDto>(park);
+            return Ok(parkDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto npDto)
+        {
+            if (npDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_npRepo.NationalParkExists(npDto.Name))
+            {
+                ModelState.AddModelError("", "National Park Exists!!!");
+                return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var npObj = _mapper.Map<NationalPark>(npDto);
+            if (!_npRepo.CreateNationalPark(npObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {npObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetNationalPark", new { nationalParkID = npObj.Id }, npObj);
+        }
+
+        [HttpPatch("{nationalParkId:int}", Name = "UpdateNationalPark")]
+        public IActionResult UpdateNationalPark(int nationalParkId, [FromBody] NationalParkDto nationalParkDto)
+        {
+            if(nationalParkDto == null || nationalParkId != nationalParkDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+            if(!_npRepo.UpdateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{nationalParkId:int}", Name = "DeleteNationalPark")]
+        public IActionResult DeleteNationalPark(int nationalParkId)
+        {
+            if(!_npRepo.NationalParkExists(nationalParkId))
+            {
+                return NotFound();
+            }
+
+            var nationalParkObj = _npRepo.GetNationalPark(nationalParkId);
+            if(!_npRepo.DeleteNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when deleting the National Park {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+
+    }
+}
